1486fc325a7b0443031b30dbb4256208acfdada2
Jonas <lehmanjo@users.noreply.github.com>
2020-06-14 16:21:22 +0800

    initial commit

---
+++ .devcontainer/user_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
+
+~/.cargo/bin/rustup set profile default 
+~/.cargo/bin/rustup self update 
+~/.cargo/bin/rustup component add rls rust-analysis 
+
+~/.cargo/bin/rustup completions bash > ~/.rustup_completions_bash 
+echo '. ~/.rustup_completions_bash' >> ~/.bashrc 
+
+sed -i 's|\(PS1=.*\@\)\\h|\1docker|g' ~/.bashrc 
+
+chmod 0700 ~/. 
+chmod 0640 ~/.bash*
+chmod 0640 ~/.profile*
