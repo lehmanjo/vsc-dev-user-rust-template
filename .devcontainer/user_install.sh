e501fd695935bef87e1facabf16904dcd88663d5
Jonas <lehmanjo@users.noreply.github.com>
2020-06-14 13:39:54 +0000

    fixed path

--- .devcontainer/user_install.sh
+++ .devcontainer/user_install.sh
@@ -8,7 +8,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 ~/.cargo/bin/rustup self update 
 ~/.cargo/bin/rustup component add rls rust-analysis 
 ~/.cargo/bin/rustup completions bash > ~/.rustup_completions_bash 
-echo 'export PATH="~/.cargo/bin:$PATH"' >> ~/.bashrc 
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc  
 echo '. ~/.rustup_completions_bash' >> ~/.bashrc 
 
 # Remove container hostname from prompt
