2589d92c050d71360f474c4e50e3d1ed65a773b7
Jonas <lehmanjo@users.noreply.github.com>
2020-06-14 17:19:18 +0800

    small fixes

--- .devcontainer/user_install.sh
+++ .devcontainer/user_install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+# Download rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Default installation, add additional components
 ~/.cargo/bin/rustup set profile default 
 ~/.cargo/bin/rustup self update 
 ~/.cargo/bin/rustup component add rls rust-analysis 
 ~/.cargo/bin/rustup completions bash > ~/.rustup_completions_bash 
+echo 'export PATH="~/.cargo/bin:$PATH"' >> ~/.bashrc 
 echo '. ~/.rustup_completions_bash' >> ~/.bashrc 
+
+# Remove container hostname from prompt
 sed -i 's|\(PS1=.*\@\)\\h|\1docker|g' ~/.bashrc 
+
+# Set proper unix file permissions
 chmod 0700 ~/. 
 chmod 0640 ~/.bash*
 chmod 0640 ~/.profile*
